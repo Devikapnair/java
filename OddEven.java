6020b3a86342b48564bd13fca24cf0d1e7700a50
Devikapnair <84074423+Devikapnair@users.noreply.github.com>
2021-10-06 16:20:40 +0530

    Add files via upload

--- OddEven.java
+++ OddEven.java
@@ -1,44 +1,26 @@
 import java.io.*;
 import java.util.Scanner;
 public class OddEven
+public static void main(String args[])throws IOException, FileNotFoundException
+{
+File f1=new file("/home/exam/abc.txt");
+FileWriter f1=new FileWriter("/home/exam/odd.txt"); 
+FileWriter f2=new FileWriter("/home/exam/even.txt");
+Scanner sc=new Scanner(f1);
+
+for(int i=1;i<=n;i++)
+{
+ int a[]=new no(i);
+ a[i]=new no();
+ read.a[i];
+ }
+ if(i%2=0)
  {
-   public static void main(String args[])
-    { 
-      int a[]=new int[5];
-     try
-      {
-       FileWriter f1=new FileWriter("C:\\Users\\devik\\Desktop\\devika\\java\\file\\odd.txt");
-       FileWriter f2=new FileWriter("C:\\Users\\devik\\Desktop\\devika\\java\\file\\even.txt");
-        FileWriter oe = new FileWriter("C:\\Users\\devik\\Desktop\\devika\\java\\file\\OodEvensfile.txt");
-       Scanner sc=new Scanner(System.in);
-       System.out.println(" enter any 5 no.s");
-       for (int i=0; i<5; i++)
-        {
-          a[i]=sc.nextInt();
-         oe.write(a[i]+"\n");
-        }
-        oe.close();
-        System.out.println("Successfully wrote to the file:");
-        String s;
-        FileReader fr=new FileReader("C:\\Users\\devik\\Desktop\\devika\\java\\file\\OodEvensfile.txt"); 
-        BufferedReader br=new BufferedReader(fr);
-       while((s=br.readLine())!=null)
-        {
-         int c=Integer.parseInt(s);
-          if(c%2 ==0)
-             f2.write(c+"\n");
-          else
-            f1.write(c+"\n");
-            
-         }
-         f1.close();
-         f2.close();
-         fr.close();
-    
-      }
-     catch (IOException e)
-      { 
-        System.out.println("An error occurred."); 
-      }
-   }
-}
+ i=f1.write();
+ }
+ System.out.println("odd no");
+ else
+ {
+ System.out.println("even no");
+ }
+ }
